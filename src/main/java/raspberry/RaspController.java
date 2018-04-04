b76fd9de13d9e598e9dc578a0c915428e13f5789
kitri <kitri@DESKTOP-NH9IC1I>
2018-04-04 16:15:04 +0900

    commit

--- src/main/java/raspberry/RaspController.java
+++ src/main/java/raspberry/RaspController.java
@@ -7,22 +7,34 @@ import java.net.URL;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RaspController {
 	@RequestMapping("/rasp.do")
-	public void raspCall() {
+	public String raspCall() {
 		String url="http://192.168.0.12:5000/rasp";
 		ModelAndView mav=new ModelAndView();
-		try {
+		/*try {
 			HttpURLConnection conn=(HttpURLConnection)new URL(url).openConnection();
-			//mav.setViewName("/raspberry/test.jsp");
+			System.out.println("test");
+			conn.setDoOutput(true);
+			conn.setDoInput(true);
+			conn.setRequestProperty("Content-Type", "application/json");
+			conn.setRequestProperty("Accept", "*");
+			conn.setRequestProperty("X-Requested-With", "XMLHttpRequest");
+			conn.setRequestMethod("GET");
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		*/
+		return "redirect:http://192.168.0.12:5000/rasp";
+	}
+	
+	@RequestMapping("/result.do")
+	public String test() {
+		return "redirect:/raspberry/test";
 	}
 }
