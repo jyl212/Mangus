49f61519026ed47641d13bd5ba77f37c8ca6f245
kitri <kitri@DESKTOP-NH9IC1I>
2018-04-13 16:30:31 +0900

    commit

--- src/main/java/raspberry/RaspController.java
+++ src/main/java/raspberry/RaspController.java
@@ -1,40 +1,54 @@
 package raspberry;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class RaspController {
 	@RequestMapping("/rasp.do")
-	public String raspCall() {
-		String url="http://192.168.0.12:5000/rasp";
-		ModelAndView mav=new ModelAndView();
-		/*try {
-			HttpURLConnection conn=(HttpURLConnection)new URL(url).openConnection();
-			System.out.println("test");
+	public String callMycrawler(){
+		int BUFFER_SIZE=1024;
+		char[] buffer=new char[BUFFER_SIZE];
+		int charsRead=0;
+		String url = "http://192.168.0.12:5000/result";
+		StringBuilder sb=new StringBuilder();  
+		try {
+			HttpURLConnection  conn = 
+					(HttpURLConnection)new URL(url).openConnection();
 			conn.setDoOutput(true);
 			conn.setDoInput(true);
-			conn.setRequestProperty("Content-Type", "application/json");
-			conn.setRequestProperty("Accept", "*");
+			conn.setRequestProperty("Content-Type", "text/html;charset=UTF-8");
+			conn.setRequestProperty("Accept", "*/*");
 			conn.setRequestProperty("X-Requested-With", "XMLHttpRequest");
 			conn.setRequestMethod("GET");
+			
+			BufferedReader br = 
+					new BufferedReader(new InputStreamReader(conn.getInputStream(),"utf-8"));  
+
+            while ((charsRead = br.read(buffer,0,BUFFER_SIZE))!=-1) {  
+             sb.append(buffer,0,charsRead);
+            }
+            System.out.println(""+sb.toString());  
+            if(sb.toString().equals("재생")) {
+            	return "redirect:/p/play.do";
+            }
 		} catch (MalformedURLException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		*/
-		return "redirect:http://192.168.0.12:5000/rasp";
+		return sb.toString();
 	}
-	
 	@RequestMapping("/result.do")
 	public String test() {
-		return "redirect:/raspberry/test";
+		return "/raspberry/test";
 	}
 }
--- src/main/webapp/WEB-INF/raspberry/test.jsp
+++ src/main/webapp/WEB-INF/raspberry/test.jsp
@@ -1,12 +1,34 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
-<!DOCTYPE>
+<!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<script type="text/javascript"
+	src="https://code.jquery.com/jquery-1.12.4.min.js"></script>
+<script type="text/javascript">
+	$(document).ready(function() {
+		$("#ajaxbtn").on("click", function() {
+			$.ajax({
+				async : true,
+				url : "/springProject/rasp.do",
+				type : "get",
+				dataType : "text",
+				success : success_run
+			});
+		});
+	});
+	function success_run(data) {
+		alert(data);
+		$("#result").text(data);
+	}
+</script>
 </head>
 <body>
-	<h1>${result}</h1>
+	<div>
+		<input type="button" id="ajaxbtn" value="test"/>
+		<p id="result"></p>
+	</div>
 </body>
 </html>
